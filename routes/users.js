a8feb6797f321ed6f51df769d0c8c8f9bfeba9d9
Madhav Bahl <madhavbahl10@gmail.com>
2018-09-02 01:04:29 +0530

    Add server side form validation

--- routes/users.js
+++ routes/users.js
@@ -9,15 +9,46 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('signup.hbs', { title: 'Register' });
+  res.render('register.hbs', { title: 'Register' });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('signup.hbs', { title: 'Login' });
+  res.render('register.hbs', { title: 'Login' });
 });
 
 router.post('/register', upload.single('profileimage'), function(req, res, next) {
-  console.log(req.body.name);
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  if (req.file) {
+    console.log('Uploading file...');
+    var profileImage = req.file.filename;
+  } else {
+    console.log('No File Uploaded');
+    var profileImage = 'noimage.jpg';
+  }
+
+  // Form Validation
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  // Check Errors
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render('register', {
+      errors: errors
+    })
+  }  else {
+    console.log('No Errors!');
+  }
 });
 
 module.exports = router;
