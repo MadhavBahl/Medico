94c47168d77d1e6209306c01e752effefc8db217
Madhav Bahl <madhavbahl10@gmail.com>
2018-09-01 21:18:24 +0530

    Add new register page

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,7 +9,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register.hbs');
+  res.render('signup.hbs', { title: 'Register' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('signup.hbs', { title: 'Login' });
+});
+
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  console.log(req.body.name);
 });
 
 module.exports = router;
